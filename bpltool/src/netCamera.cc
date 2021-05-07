7662a53d49b9e69735f73e5008dae4f7bcd2b545
z-qh <1039903166@qq.com>
2021-05-07 16:49:28 +0800

    安保机器人前后相机的ROS驱动，使用Opencv拉流的方法

--- bpltool/src/netCamera.cc
+++ bpltool/src/netCamera.cc
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include "opencv2/opencv.hpp"
 #include "sensor_msgs/Image.h"
+#include "cv_bridge/cv_bridge.h"
 
 #include <thread>
 #include <iostream>
@@ -14,9 +15,42 @@ using namespace std;
 using namespace cv;
 
 mutex mut;
-queue<Mat> imgQueue;
+queue<Mat> imgQueueFront;
+queue<Mat> imgQueueBack;
 
-void getImageStream()
+ros::Publisher pubImgFront;
+ros::Publisher pubImgBack;
+
+void getImageStreamFront()
+{
+    VideoCapture cap;
+    Mat tempImg;
+    string camAdd = "rtsp://admin:Aa123456@192.168.1.63/h264/ch1/main/av_stream";
+    try
+    {
+        cap.open(0);
+    }
+    catch (int e)
+    {
+        mut.lock();
+        cout << e << endl;
+        mut.unlock();
+    }
+    while(1)
+    {
+        cap >> tempImg;
+        if(tempImg.empty())
+            continue;
+        mut.lock();
+        if(imgQueueFront.size() >= 5)
+            imgQueueFront.pop();
+        imgQueueFront.push(tempImg);
+        mut.unlock();
+        usleep(5000);//5ms
+    }
+}
+
+void getImageStreamBack()
 {
     VideoCapture cap;
     Mat tempImg;
@@ -31,24 +65,17 @@ void getImageStream()
         cout << e << endl;
         mut.unlock();
     }
-    while(cap.isOpened() == false)
-    {
-        mut.lock();
-        cout << "open error, trying!!!" << endl;
-        mut.unlock();
-        cap.open(camAdd);
-    }
     while(1)
     {
         cap >> tempImg;
         if(tempImg.empty())
             continue;
         mut.lock();
-        if(imgQueue.size() >= 20)
-            imgQueue.pop();
-        imgQueue.push(tempImg);
+        if(imgQueueBack.size() >= 5)
+            imgQueueBack.pop();
+        imgQueueBack.push(tempImg);
         mut.unlock();
-        usleep(10000);//10ms
+        usleep(5000);//5ms
     }
 }
 
@@ -58,24 +85,38 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "netCam");
     ros::NodeHandle nh;
 
+    pubImgFront = nh.advertise<sensor_msgs::Image>("/frontImg", 1);
+    pubImgBack = nh.advertise<sensor_msgs::Image>("/backImg", 1);
+
     ros::Rate loop(30);
 
     Mat img;
 
-    thread netCamGetThread(getImageStream);
+    thread netCamGetThreadFront(getImageStreamFront);
+    thread netCamGetThreadBack(getImageStreamBack);
 
-    netCamGetThread.detach();
+    netCamGetThreadFront.detach();
+    netCamGetThreadBack.detach();
 
     while (ros::ok())
     {
-        if(!imgQueue.empty())
+        if(!imgQueueFront.empty())
         {
-            img = imgQueue.back();
-            imshow("src", img);
-            waitKey(10);
+            sensor_msgs::Image ROSImgFront = *cv_bridge::CvImage(std_msgs::Header(), "bgr8", imgQueueFront.back()).toImageMsg();
+
+            ROSImgFront.header.frame_id = "/map";
+            ROSImgFront.header.stamp = ros::Time::now();
+            pubImgFront.publish(ROSImgFront);
+        }
+        if(!imgQueueBack.empty())
+        {
+            sensor_msgs::Image ROSImgBack = *cv_bridge::CvImage(std_msgs::Header(), "bgr8", imgQueueBack.back()).toImageMsg();
+
+            ROSImgBack.header.frame_id = "/map";
+            ROSImgBack.header.stamp = ros::Time::now();
+            pubImgBack.publish(ROSImgBack);
         }
         loop.sleep();
     }
-
     return 0;
 }

de91768939d358e5ae26deaeb5277c84e0ecf0de
qh-z <1039903166@qq.com>
2022-05-11 17:01:24 +0800

    update

---
+++ selo/src/selo.cc
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include "rosbag/bag.h"
+#include "rosbag/view.h"
+
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+string sourceBagPath;
+
+int main(int argc, char** argv)
+{
+    return 0;
+}
+
